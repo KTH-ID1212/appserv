fae0c2a293050488b3c97a4efbc266776be82e37
Leif Lindbäck <leifl@kth.se>
2017-10-11 10:49:56 +0200

    Introduced integration layer.

--- src/main/java/se/kth/id1212/appserv/bank/controller/CashierFacade.java
+++ src/main/java/se/kth/id1212/appserv/bank/controller/CashierFacade.java
@@ -23,12 +23,12 @@
  */
 package se.kth.id1212.appserv.bank.controller;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
-import javax.persistence.PersistenceContext;
+import se.kth.id1212.appserv.bank.integration.BankDAO;
 import se.kth.id1212.appserv.bank.model.Account;
 import se.kth.id1212.appserv.bank.model.AccountDTO;
 import se.kth.id1212.appserv.bank.model.OverdraftException;
@@ -40,9 +40,7 @@ import se.kth.id1212.appserv.bank.model.OverdraftException;
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 @Stateless
 public class CashierFacade {
-    @PersistenceContext(unitName = "bankPU")
-    private EntityManager em;
-
+    @EJB BankDAO bankDB;
     /**
      * Creates a new account with the specified data.
      *
@@ -52,7 +50,7 @@ public class CashierFacade {
      */
     public AccountDTO createAccount(String firstName, String lastName, int balance) {
         Account newAcct = new Account(balance, firstName, lastName);
-        em.persist(newAcct);
+        bankDB.storeAccount(newAcct);
         return newAcct;
     }
 
@@ -64,11 +62,7 @@ public class CashierFacade {
      * @throws EntityNotFoundException If the account was not found.
      */
     public AccountDTO findAccount(int acctNo) {
-        AccountDTO found = em.find(Account.class, acctNo);
-        if (found == null) {
-            throw new EntityNotFoundException("No account with number " + acctNo);
-        }
-        return found;
+        return bankDB.findAccountByAcctNo(acctNo);
     }
 
     /**
@@ -78,7 +72,7 @@ public class CashierFacade {
      * @throws OverdraftException If withdrawal would result in a negative balance.
      */
     public void withdraw(int acctNo, int amount) throws OverdraftException {
-        Account acct = em.find(Account.class, acctNo);
+        Account acct = bankDB.findAccountByAcctNo(acctNo);
         acct.withdraw(amount);
     }
 
@@ -88,7 +82,7 @@ public class CashierFacade {
      * @param amount The amount to deposit.
      */
     public void deposit(int acctNo, int amount) {
-        Account acct = em.find(Account.class, acctNo);
+        Account acct = bankDB.findAccountByAcctNo(acctNo);
         acct.deposit(amount);
     }
 
